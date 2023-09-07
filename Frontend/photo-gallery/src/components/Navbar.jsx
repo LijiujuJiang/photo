fca13f71c6cb807cba2be79aa84995c0ec5c5297
LijiujuJiang <852701573@qq.com>
2023-09-07 17:37:29 +1000

    1
    111

---
+++ Frontend/photo-gallery/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import navItems from "./img";
+
+
+export default function Navbar() {
+  return (
+    <>
+      <div style={{ height: "500px" }}>
+        <p style={{ textAlign: "center", margin: "10px" }}>枫丹摄影展</p>
+        <Carousel
+          showArrows={true}
+          showStatus={false}
+          showIndicators={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          stopOnHover={true}
+          dynamicHeight={false}
+        >
+          {navItems.map((item, index) => (
+            <div key={index}>
+              <img src={item.imageUrl} />
+              <p style={{ marginTop: "10px",color:item.color}}>{item.title}</p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
+  );
+}
